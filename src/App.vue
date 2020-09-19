ee1012ba5b539c46364f4683086e39ac186274c5
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-09-19 15:11:18 +0000

    components, binding, computed properties and methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img src="./assets/build-a-bot-logo.png" class="logo" alt="">
+            Build a bot
+          </li>
+          <li>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+   <main>
+     <!-- <HomePage msg="Welcome to Your Vue.js App"/> -->
+     <RobotBuild />
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuild from './build/RobotBuild.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HomePage,
+    RobotBuild,
   },
 };
 </script>
@@ -25,4 +44,26 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+  list-style-type: none
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+
 </style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true">
+    </div>
+    <div class="get-started">
+      <a href="">Get Started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+body{
+  background: linear-gradient(to bottom, #555,#999);
+  background-attachment: fixed;
+}
+
+#app{
+  font-family: 'Avenir',Helvetica, Arial, sans-serif;
+}
+main{
+  margin: 0 auto;
+  padding: 30px;
+  background: white;
+  width: 1024px;
+  min-height:300px;
+}
+
+.home{
+  text-align: center;
+}
+
+.robot{
+  height: 300px;
+}
+</style>
